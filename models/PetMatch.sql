ee5ed3ae0d40e18f27a532cf269031a41a7bd990
Emiliano Aquetzalli Obregon Reyes <eobregon05@alumnos.uaq.mx>
2023-12-07 11:40:38 -0600

    set database

--- models/PetMatch.sql
+++ models/PetMatch.sql
@@ -1,3 +1,4 @@
+-- Table users
 CREATE TABLE users (
   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   rol ENUM('0', '1') DEFAULT '0',
@@ -13,11 +14,12 @@ CREATE TABLE users (
   phone_Number VARCHAR(255) NOT NULL
 );
 
+-- Table pets
 CREATE TABLE pets (
   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   name VARCHAR(255) NOT NULL,
   breed VARCHAR(255) NOT NULL,
-  tipe VARCHAR(255) NOT NULL,
+  type VARCHAR(255) NOT NULL,
   pet_Size VARCHAR(20) NOT NULL,
   age INT,
   description TEXT NOT NULL,
@@ -27,9 +29,9 @@ CREATE TABLE pets (
   status VARCHAR(255)
 );
 
+-- Table images_posts
 CREATE TABLE images_posts (
-  id INT PRIMARY KEY NOT NULL,
-  post_id INT NOT NULL,
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   filename VARCHAR(255) NOT NULL,
   path VARCHAR(255) NOT NULL,
   originalname VARCHAR(255) NOT NULL,
@@ -37,8 +39,9 @@ CREATE TABLE images_posts (
   size INT NOT NULL
 );
 
+-- Table images_profile
 CREATE TABLE images_profile (
-  id INT PRIMARY KEY NOT NULL,
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   user_id INT NOT NULL,
   filename VARCHAR(255) NOT NULL,
   path VARCHAR(255) NOT NULL,
@@ -47,6 +50,7 @@ CREATE TABLE images_profile (
   size INT NOT NULL
 );
 
+-- Table posts_community
 CREATE TABLE posts_community (
   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   user_id INT,
@@ -54,6 +58,7 @@ CREATE TABLE posts_community (
   Date DATE
 );
 
+-- Table comments_community
 CREATE TABLE comments_community (
   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   post_id INT NOT NULL,
@@ -62,17 +67,17 @@ CREATE TABLE comments_community (
   date DATE NOT NULL
 );
 
+-- Table posts
 CREATE TABLE posts (
   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   date DATE,
   pet INT,
   img INT,
-  comments INT,
-  tags INT
+  status INT DEFAULT 0
 );
 
+-- Table preferences
 CREATE TABLE preferences (
-  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   user_Id INT,
   housing_Type ENUM('Pequeno','Mediano','Grande'),
   allergies ENUM('Yes', 'No'),
@@ -82,27 +87,12 @@ CREATE TABLE preferences (
   weather ENUM('Frio', 'Calor', 'Templado', 'Todos los climas')
 );
 
-CREATE TABLE matchs (
-  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  user INT,
-  pet INT,
-  state VARCHAR(255)
-);
-
-CREATE TABLE tags (
-  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  name_tag VARCHAR(255),
-  post INT
-);
-
-ALTER TABLE tags ADD CONSTRAINT fk_tags_posts FOREIGN KEY (post) REFERENCES posts(id);
-ALTER TABLE comments_community ADD CONSTRAINT fk_comments_community_users FOREIGN KEY (user_id) REFERENCES users(id);
-ALTER TABLE comments_community ADD CONSTRAINT fk_comments_community_posts_community FOREIGN KEY (post_id) REFERENCES posts_community(id);
-ALTER TABLE posts_community ADD CONSTRAINT fk_posts_community_users FOREIGN KEY (user_id) REFERENCES users(id);
-ALTER TABLE images_profile ADD CONSTRAINT fk_images_profile_users FOREIGN KEY (user_id) REFERENCES users(id);
-ALTER TABLE images_posts ADD CONSTRAINT fk_images_posts_posts FOREIGN KEY (post_id) REFERENCES posts(id);
-ALTER TABLE pets ADD CONSTRAINT fk_pets_users FOREIGN KEY (owner) REFERENCES users(id);
-ALTER TABLE posts ADD CONSTRAINT fk_posts_pets FOREIGN KEY (pet) REFERENCES pets(id);
-ALTER TABLE preferences ADD CONSTRAINT fk_preferences_users FOREIGN KEY (user_Id) REFERENCES users(id);
-ALTER TABLE matchs ADD CONSTRAINT fk_matchs_users FOREIGN KEY (user) REFERENCES users(id);
-ALTER TABLE matchs ADD CONSTRAINT fk_matchs_pets FOREIGN KEY (pet) REFERENCES pets(id);
+-- Referencias
+ALTER TABLE comments_community ADD FOREIGN KEY (user_id) REFERENCES users(id);
+ALTER TABLE comments_community ADD FOREIGN KEY (post_id) REFERENCES posts_community(id);
+ALTER TABLE posts_community ADD FOREIGN KEY (user_id) REFERENCES users(id);
+ALTER TABLE images_profile ADD FOREIGN KEY (user_id) REFERENCES users(id);
+ALTER TABLE images_posts ADD FOREIGN KEY (id) REFERENCES posts(img);
+ALTER TABLE pets ADD FOREIGN KEY (owner) REFERENCES users(id);
+ALTER TABLE posts ADD FOREIGN KEY (pet) REFERENCES pets(id);
+ALTER TABLE preferences ADD FOREIGN KEY (user_Id) REFERENCES users(id);
